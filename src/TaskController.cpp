74b67f9b58e8d86305b0d3264aeca96600d8a8ae
y-shiigi <y.shiigi@thk.co.jp>
2019-12-07 20:02:34 +0900

    parameter tuning

--- src/TaskController.cpp
+++ src/TaskController.cpp
@@ -19,10 +19,18 @@ bool TaskController::moveMarker(std::string _marker){
   reached_x = reached_y = reached_th = false;
   rotate.setRPY(0,1.57,0);
 
+  double goal_x = 0.01;
+  double goal_y = 0.01;
+  double goal_z = 0.01;
+
+  double gain_x = 0.3;
+  double gain_y = 0.3;
+  double gain_z = 0.2;
+
   while(1){
     //marker_coord = GetTransformObject("ar_marker_0");
     marker_coord = GetTransformObject(_marker);
-    camera_coord = GetTransformObject("camera_link");
+    camera_coord = GetTransformObject("front_camera_color_frame");
 
     dx = marker_coord.getOrigin().x() - camera_coord.getOrigin().x();
     dy = marker_coord.getOrigin().y() - camera_coord.getOrigin().y();
@@ -31,23 +39,23 @@ bool TaskController::moveMarker(std::string _marker){
     //std::cout << dx << "\t" << dy << "\t" << fabs(dx) << "\t" << fabs(dy) << std::endl;
     //std::cout << "\t" << dth << std::endl;
 
-    if(fabs(dx) < 0.20) reached_x = true;
+    if(fabs(dx) < goal_x) reached_x = true;
     else reached_x = false;
-    if(fabs(dy) < 0.01) reached_y = true;
+    if(fabs(dy) < goal_y) reached_y = true;
     else reached_y = false;
-    if(fabs(dth) < 0.02) reached_th = true;
+    if(fabs(dth) < goal_z) reached_th = true;
     else reached_th = false;
 
 
     if((marker_coord.getOrigin() == tf::Vector3(0.0,0.0,0.0)) || (reached_x == true && reached_y == true && reached_th == true)) break;
     else{
-      if(reached_x == false) cmd_vel_.linear.x = dx * 0.1;
+      if(reached_x == false) cmd_vel_.linear.x = dx * gain_x;
       else cmd_vel_.linear.x = 0;
 
-      if(reached_y == false) cmd_vel_.linear.y = dy * 0.3;
+      if(reached_y == false) cmd_vel_.linear.y = dy * gain_y;
       else cmd_vel_.linear.y = 0;
 
-      if(reached_th == false) cmd_vel_.angular.z= dth * 0.2;
+      if(reached_th == false) cmd_vel_.angular.z= dth * gain_z;
       else cmd_vel_.angular.z= 0;
 
       cmd_vel_pub_.publish(cmd_vel_);
